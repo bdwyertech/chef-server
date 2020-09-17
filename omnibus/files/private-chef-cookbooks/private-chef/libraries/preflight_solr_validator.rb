1a4115835bb35ea9047f470dfbebb8dc24eab818
Steven Danna <steve@chef.io>
2020-09-17 17:21:10 +0100

    cookbooks: remove defunct solr preflight checks
    Signed-off-by: Steven Danna <steve@chef.io>

--- omnibus/files/private-chef-cookbooks/private-chef/libraries/preflight_solr_validator.rb
+++ omnibus/files/private-chef-cookbooks/private-chef/libraries/preflight_solr_validator.rb
@@ -18,11 +18,10 @@ require_relative './preflight_checks.rb'
 class SolrPreflightValidator < PreflightValidator
   # The cs_*attr variables hold the user-defined configuration
   attr_reader :cs_solr_attr, :cs_erchef_attr,
-              :cs_elasticsearch_attr, :cs_rabbitmq_attr,
-              :cs_opscode_expander_attr
+              :cs_elasticsearch_attr
 
   # The node_*attr variables hold the default configuration
-  attr_reader :node_solr_attr, :node_elasticsearch_attr, :node_erchef_attr
+  attr_reader :node_elasticsearch_attr
 
   def initialize(node)
     super
@@ -30,68 +29,26 @@ class SolrPreflightValidator < PreflightValidator
     @cs_solr_attr = PrivateChef['opscode_solr4']
     @cs_erchef_attr = PrivateChef['opscode_erchef']
     @cs_elasticsearch_attr = PrivateChef['elasticsearch']
-    @cs_rabbitmq_attr = PrivateChef['rabbitmq']
-    @cs_opscode_expander_attr = PrivateChef['opscode_expander']
 
-    @node_erchef_attr = node['private_chef']['opscode-erchef']
-    @node_solr_attr = node['private_chef']['opscode-solr4']
     @node_elasticsearch_attr = node['private_chef']['elasticsearch']
   end
 
   def run!
-    verify_one_search_provider
-    verify_es_disabled_if_user_set_external_solr
-    verify_unused_services_are_disabled_if_using_internal_es
-
     warn_unchanged_external_flag
+
+    verify_es_disabled_if_user_set_external_solr
     verify_external_url
     verify_erchef_config
   end
 
-  def verify_one_search_provider
-    if elasticsearch_enabled? && solr_enabled?
-      fail_with err_SOLR002_failed_validation
-    end
-  end
-
-  # If an external search provider (ES or Solr) was explicitly enabled
-  # by the user, then we expect that both internal search services
-  # should be disabled.
+  # If an external search provider was explicitly enabled by the user,
+  # then we expect that both internal search services should be
+  # disabled.
   def verify_es_disabled_if_user_set_external_solr
     # NOTE(ssd) 2020-05-13: This might impact the people who we gave
     # pre-release access to.
-    if cs_solr_attr['external'] && elasticsearch_enabled?
+    if external? && elasticsearch_enabled?
       fail_with err_SOLR003_failed_validation
-    end
-
-    if solr_enabled? && external?
-      fail_with err_SOLR004_failed_validation
-    end
-  end
-
-  # The user might have a combination of solr, rabbitmq or
-  # opscode-expander explicitly enabled in their config.
-  #
-  # If we are using the new internal elasticsearch, this is an
-  # unsupported configuration.
-  #
-  def verify_unused_services_are_disabled_if_using_internal_es
-    if elasticsearch_enabled?
-      msg = []
-      if cs_solr_attr['enable']
-        msg << err_SOLR005_failed_validation
-      end
-
-      if cs_rabbitmq_attr['enable']
-        msg << err_SOLR006_failed_validation
-      end
-
-      if cs_opscode_expander_attr['enable']
-        msg << err_SOLR007_failed_validation
-      end
-      unless msg.empty?
-        fail_with msg.join("\n")
-      end
     end
   end
 
@@ -110,14 +67,6 @@ class SolrPreflightValidator < PreflightValidator
       node_elasticsearch_attr['enable']
     else
       cs_elasticsearch_attr['enable']
-    end
-  end
-
-  def solr_enabled?
-    if cs_solr_attr['enable'].nil?
-      node_solr_attr['enable']
-    else
-      cs_solr_attr['enable']
     end
   end
 
@@ -163,26 +112,9 @@ class SolrPreflightValidator < PreflightValidator
     provider = @cs_erchef_attr['search_provider']
     return true if provider.nil? # default provider
 
-    case provider
-    when 'elasticsearch'
-      unless %w(batch inline).include?(@cs_erchef_attr['search_queue_mode'])
-        fail_with err_SOLR011_failed_validation
-      end
-    when 'solr'
-    else
-      fail_with err_SOLR012_failed_validation
+    unless %w(batch inline).include?(@cs_erchef_attr['search_queue_mode'])
+      fail_with err_SOLR011_failed_validation
     end
-  end
-
-  def err_SOLR002_failed_validation
-    <<~EOM
-
-      SOLR002: The #{CHEF_SERVER_NAME} is configured to enable both
-               Elasticsearch and Solr. This is an unsupported configuration.
-
-               Please update #{CHEF_SERVER_CONFIG_FILE} such that only one
-               search provider is enabled.
-      EOM
   end
 
   def err_SOLR003_failed_validation
@@ -200,71 +132,6 @@ class SolrPreflightValidator < PreflightValidator
                entry for opscode_solr4['external'].
 
      EOM
-  end
-
-  def err_SOLR004_failed_validation
-    <<~EOM
-
-      SOLR004: The #{CHEF_SERVER_NAME} is configured to use an external search
-               provider but the internal Solr is still enabled. This is an
-               unsupported configuration.
-
-               To disable the internal Solr, add the following to #{CHEF_SERVER_CONFIG_FILE}:
-
-                   opscode_solr4['enable'] = false
-
-               Alternatively, if you are attempting to use the internal Solr,
-               add the following to #{CHEF_SERVER_CONFIG_FILE}:
-
-                   opscode_solr4['enable'] = true
-                   opscode_solr4['external'] = false
-
-    EOM
-  end
-
-  def err_SOLR005_failed_validation
-    <<~EOM
-
-      SOLR005: The #{CHEF_SERVER_NAME} is configured to use its internal
-               Elasticsearch installation, but opscode_solr4 has been manually
-               enabled. This is an unsupported configuration.
-
-               Please update #{CHEF_SERVER_CONFIG_FILE} to ensure that
-               opscode_solr4 is disabled:
-
-                   opscode_solr4['enable'] = false
-
-    EOM
-  end
-
-  def err_SOLR006_failed_validation
-    <<~EOM
-
-      SOLR006: The #{CHEF_SERVER_NAME} is configured to use its internal
-               Elasticsearch installation, but RabbitMQ has been manually
-               enabled. This is an unsupported configuration.
-
-               Please update #{CHEF_SERVER_CONFIG_FILE} to ensure that
-               rabbitmq is disabled:
-
-                   rabbitmq['enable'] = false
-
-    EOM
-  end
-
-  def err_SOLR007_failed_validation
-    <<~EOM
-
-      SOLR007: The #{CHEF_SERVER_NAME} is configured to use its internal
-               Elasticsearch installation, but opscode-expander has been
-               manually enabled. This is an unsupported configuration.
-
-               Please update #{CHEF_SERVER_CONFIG_FILE} to ensure that
-               opscode-expander is disabled:
-
-                   opscode_expander['enable'] = false
-
-    EOM
   end
 
   def err_SOLR009_warn_validation
@@ -317,16 +184,6 @@ class SolrPreflightValidator < PreflightValidator
 
                in #{CHEF_SERVER_CONFIG_FILE}
 
-    EOM
-  end
-
-  def err_SOLR012_failed_validation
-    <<~EOM
-      SOLR012: The specified search provider (#{provider}) is not currently supported.
-               Please choose from one of the following search providers:
-
-               solr
-               elasticsearch
     EOM
   end
 end
